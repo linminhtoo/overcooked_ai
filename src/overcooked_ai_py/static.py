46e845ba470b3fcdeb7aa4041abaccb76ea2ed75
Min Htoo <linmin.htoo@gmail.com>
2023-11-30 21:41:49 +0800

    feat: add random string

--- src/overcooked_ai_py/static.py
+++ src/overcooked_ai_py/static.py
@@ -1,9 +1,15 @@
 import os
+import random
+import string
+
+
+random_string = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(20))
+print(f"RANDOM STRING: {random_string}")
 
 _current_dir = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(_current_dir, "data")
 HUMAN_DATA_DIR = os.path.join(DATA_DIR, "human_data")
-PLANNERS_DIR = os.path.join(DATA_DIR, "planners")
+PLANNERS_DIR = os.path.join(DATA_DIR, f"planners_{random_string}")
 LAYOUTS_DIR = os.path.join(DATA_DIR, "layouts")
 GRAPHICS_DIR = os.path.join(DATA_DIR, "graphics")
 FONTS_DIR = os.path.join(DATA_DIR, "fonts")
