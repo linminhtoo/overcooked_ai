ed6812c7da86198331fec935536320d2e3f240f2
Min Htoo <linmin.htoo@gmail.com>
2023-11-30 21:52:46 +0800

    fix: create dir

--- src/overcooked_ai_py/static.py
+++ src/overcooked_ai_py/static.py
@@ -1,6 +1,7 @@
 import os
 import random
 import string
+from pathlib import Path
 
 
 random_string = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(20))
@@ -10,6 +11,7 @@ _current_dir = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(_current_dir, "data")
 HUMAN_DATA_DIR = os.path.join(DATA_DIR, "human_data")
 PLANNERS_DIR = os.path.join(DATA_DIR, f"planners_{random_string}")
+Path(PLANNERS_DIR).mkdir(parents=True, exist_ok=True)
 LAYOUTS_DIR = os.path.join(DATA_DIR, "layouts")
 GRAPHICS_DIR = os.path.join(DATA_DIR, "graphics")
 FONTS_DIR = os.path.join(DATA_DIR, "fonts")
